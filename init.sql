436b123e2fc3dbb3be626faf63bd65b37cf1b04a
OpalinskiJakub <jakopal1125@gmail.com>
2024-02-14 21:57:23 +0100

    build: init.sql update

--- init.sql
+++ init.sql
@@ -1,6 +1,14 @@
 CREATE TABLE IF NOT EXISTS users (
-                                     id INT AUTO_INCREMENT PRIMARY KEY,
-                                     username VARCHAR(255) UNIQUE,
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    username VARCHAR(255) UNIQUE,
     hashed_password BLOB,
     salt BLOB
+    ) DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+CREATE TABLE IF NOT EXISTS posts (
+    postId INT AUTO_INCREMENT PRIMARY KEY,
+    postTitle VARCHAR(255),
+    postContent TEXT,
+    authorId INT,
+    FOREIGN KEY (authorId) REFERENCES users(id)
     ) DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
